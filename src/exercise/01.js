cfe263bdd11da774f3238538720b672442447dd6
dawlatzai <dawlatzai.ghousi@gmail.com>
2022-04-10 22:45:09 +0430

    Solve first challenge 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,23 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
+function Counter({initialCount = 0, step = 10}) {
   const countReducer = (previousCount, newCount) => {
     return newCount
   }
 
   const [count, setCount] = React.useReducer(countReducer, initialCount)
+
   const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
 
 function App() {
-  return <Counter />
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
 }
 
 export default App
